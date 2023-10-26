ade0a393b2e3062330a5280e7c245dd8dc9d1d0c
Linnéa Ivansson (External) <linnea.ivansson@knowit.se>
2023-10-26 11:52:54 +0200

    Another floor fix

--- project/small_func.py
+++ project/small_func.py
@@ -1,2 +1,6 @@
 def func(x):
     return x + 1
+
+
+def another_func(x):
+    return x + 2
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,9 @@
 from project import small_func
 
 
-def test_answer():
+def test_small_func_func():
     assert small_func.func(3) == 4
+
+
+def test_small_func_another_func():
+    assert small_func.another_func(3) == 5
